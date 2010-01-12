60f85ebc97177d43e72d74c3f8ecd6cb56eb360e
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-01-12 19:01:45 +0100

    network-icdf: almost there with spanning trees

--- src/main/scala/org/drugis/SpanningTree.scala
+++ src/main/scala/org/drugis/SpanningTree.scala
@@ -10,4 +10,27 @@ object SpanningTreeEnumerator {
 	 * Enumerate all spanning trees of the undirected graph g.
 	 */
 	def treeEnumerator[T](g: UndirectedGraph[T]): Iterable[Graph[T]] = null
+
+	private def grow[T](g: Graph[T], t: Graph[T], f: List[(T, T)],
+			complete: Graph[T] => Boolean) = {
+		if (complete(t)) List(t)
+		else {
+			val d = deepen(g, t, f.tail, f.head, complete)
+			val l = d.head // last generated tree
+			if (bridgeTest(l, e)) d
+			else grow(g.remove(e), t, f.tail) :: d
+		}
+	}
+
+	private def deepen(g: Graph[T], t: Graph[T], f: List[(T, T)], e: (T, T),
+			complete: Graph[T] => Boolean) = {
+		val v = e._1
+		val t1 = t.add(e)
+		val f1 = (g.edgesFrom(v).filter(x => !t.vertexSet.contains(x._1)) ::: f).removeAll(x => x._2 == v)
+		grow(g, t1, f1, complete)
+	}
+
+	private def bridgeTest(t: Graph[T], e: (T, T)) = {
+		// this is fucked
+	}
 }
