d475f211d4e126db4ff5fe76f68435a2fb372b99
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-01-26 17:28:43 +0100

    Some changes to network.scala script (??)

--- network.scala
+++ network.scala
@@ -19,64 +19,7 @@ val best = network.bestSpanningTree(top)
 println("BEST: " + best + " has ICDF = " + network.countInconsistencies(best))
 
 
-class BaselineSearchState(
-	val toCover: Set[(Treatment, Treatment)],
-	val studies: List[Study],
-	val assignment: Map[Study, Treatment]) { }
-
-class BaselineSearchProblem(
-	toCover: Set[(Treatment, Treatment)],
-	studies: Set[Study]) extends SearchProblem[BaselineSearchState] {
-
-	val initialState = new BaselineSearchState(toCover, studies.toList,
-		Map[Study, Treatment]())
-
-	def isGoal(s: BaselineSearchState): Boolean = {
-		s.toCover.isEmpty && s.studies.isEmpty
-	}
-
-	def successors(s: BaselineSearchState): List[BaselineSearchState] = {
-		if (s.studies.isEmpty) Nil
-		else {
-			val study = s.studies.head
-			(for {t <- study.treatments.toList.sort((a, b) => a < b)
-				val toCover = s.toCover -- (
-					study.treatmentGraph.edgeSet.filter(e => (e._1 == t || e._2 == t)))
-				val assignment = s.assignment + ((study, t))
-			} yield new BaselineSearchState(toCover, s.studies.tail, assignment)
-			).toList
-		}
-	}
-}
-
-def assignMultiArm(toCover: Set[(Treatment, Treatment)], studies: Set[Study])
-: Map[Study, Treatment] = {
-	val problem = new BaselineSearchProblem(toCover, studies)
-	val alg = new DFS()
-	alg.search(problem) match {
-		case None => throw new Exception("No Assignment Found!")
-		case Some(x) => x.assignment
-	}
-}
-	
-
-def assignBaselines(network: Network, st: Tree[Treatment])
-: Map[Study, Treatment] = {
-	val toCover = network.inconsistencies(best).flatMap(a => a.edgeSet)
-	val twoArm = network.studies.filter(study => study.treatments.size == 2)
-	val multiArm = network.studies -- twoArm
-	val covered = twoArm.flatMap(study => study.treatmentGraph.edgeSet)
-
-	val twoArmMap = Map[Study, Treatment]() ++ twoArm.map(study => (study, study.treatments.toList.sort((a, b) => a < b).head))
-
-	val leftToCover = toCover -- covered
-	twoArmMap ++ assignMultiArm(leftToCover, multiArm)
-}
-
 println()
-
-def indexMap[T](l: List[T]): Map[T, Int] =
-	Map[T, Int]() ++ l.map(a => (a, l.indexOf(a) + 1))
 
 val studyList = network.studies.toList.sort((a, b) => a.id < b.id)
 val studyMap = indexMap(studyList)
