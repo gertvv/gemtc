5934e476e5f81da08baa420c2e4706cfbba871b8
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-03-10 13:11:00 +0100

    Yadas succes probability argumentmaker

--- src/main/scala/org/drugis/mtc/yadas/SuccessProbabilityArgumentMaker.scala
+++ src/main/scala/org/drugis/mtc/yadas/SuccessProbabilityArgumentMaker.scala
@@ -7,8 +7,8 @@ import gov.lanl.yadas.ArgumentMaker
  * ArgumentMaker for individual treatment success probabilities within studies.
  * p_i,k = ilogit(theta_i,k) ; theta_i,k = mu_i + delta_i,b(i),k
  */
-class SuccessProbabilityArgumentMaker(sIdx: Int, dIdx: Int,
-		model: NetworkModel)
+class SuccessProbabilityArgumentMaker(model: NetworkModel,
+		sIdx: Int, dIdx: Int)
 extends ArgumentMaker {
 	/**
 	 * Calculate "the argument": an array of succes-probabilities, one for
@@ -16,5 +16,32 @@ extends ArgumentMaker {
 	 * data[sIdx] should contain study baseline means, one per study
 	 * data[dIdx] should contain relative effects, one for each non-baseline
 	 */
-	def getArgument(data: Array[Array[Double]]): Array[Double] = null
+	def getArgument(data: Array[Array[Double]]): Array[Double] = {
+		Array.make(0, 0.0) ++ {
+			for {d <- model.data} yield prob(d._1, d._2.treatment, data)
+		}
+	}
+
+	private def ilogit(x: Double): Double = 1 / (1 + Math.exp(-x))
+
+	private def relativeTreatmentIndex(s: Study, t: Treatment): Int = {
+		model.studyRelativeEffects(s).findIndexOf(x => x._2 == t)
+	}
+
+	private def treatmentIndex(s: Study, t: Treatment): Int = {
+		val base = model.relativeEffectIndex(s)
+		if (model.studyBaseline(s) == t) -1
+		else base + relativeTreatmentIndex(s, t)
+	}
+
+	private def prob(s: Study, t: Treatment, data: Array[Array[Double]])
+	: Double = {
+		val baselineIdx = model.studyList.indexOf(s)
+		val treatmentIdx = treatmentIndex(s, t)
+
+		if (treatmentIdx >= 0) 
+			ilogit(data(sIdx)(baselineIdx) + data(dIdx)(treatmentIdx))
+		else
+			ilogit(data(sIdx)(baselineIdx))
+	}
 }
---
+++ src/test/scala/org/drugis/mtc/yadas/SuccessProbabilityArgumentMaker.scala
@@ -0,0 +1,52 @@
+package org.drugis.mtc.yadas
+
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+import org.drugis.mtc._
+
+class SuccessProbabilityArgumentMakerTest extends ShouldMatchersForJUnit {
+	val ta = new Treatment("A")
+	val tb = new Treatment("B")
+	val tc = new Treatment("C")
+	val td = new Treatment("D")
+	val treatmentList = List(ta, tb, tc, td)
+
+	val study1 = new Study("1", Map[Treatment, Measurement](
+		(ta, new Measurement(ta, 0, 1)), (tb, new Measurement(tb, 0, 1))))
+	val study2 = new Study("2", Map[Treatment, Measurement](
+		(tb, new Measurement(tb, 0, 1)), (tc, new Measurement(tc, 0, 1)), (td, new Measurement(td, 0, 1))))
+	val study3 = new Study("3", Map[Treatment, Measurement](
+		(ta, new Measurement(ta, 0, 1)), (td, new Measurement(td, 0, 1))))
+	val study4 = new Study("4", Map[Treatment, Measurement](
+		(tc, new Measurement(tc, 0, 1)), (td, new Measurement(td, 0, 1))))
+	val studyList = List(study1, study2, study3, study4)
+
+	val network = new Network(
+		Set[Treatment]() ++ treatmentList,
+		Set[Study]() ++ studyList)
+
+	val spanningTree = new Tree[Treatment](
+		Set((ta, tb), (tb, tc), (ta, td)), ta)
+
+	val networkModel = new NetworkModel(network,
+		new FundamentalGraphBasis(network.treatmentGraph, spanningTree),
+		Map[Study, Treatment](
+			(study1, ta), (study2, tb), (study3, ta), (study4, tc)),
+		treatmentList, studyList)
+
+	def ilogit(x: Double) = 1 / (1 + Math.exp(-x))
+
+	@Test def testGetArgument() {
+		val baselines = Array(1.0, 2.0, 1.5, 3.0)
+		val deltas = Array(-1.0, 3.0, 2.0, 1.0, -1.0)
+		val thetas = Array(1.0, 0.0, 2.0, 5.0, 4.0, 1.5, 2.5, 3.0, 2.0)
+		val expected = thetas.map(x => ilogit(x)).toList
+
+		
+		val maker0 = new SuccessProbabilityArgumentMaker(networkModel, 0, 1)
+		maker0.getArgument(Array(baselines, deltas)).toList should be (expected)
+	}
+}
