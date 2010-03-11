457197727dcf9cb5a061864cf63e71d9cdaa2014
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-03-11 16:50:03 +0100

    start with continuous data meta-analysis

---
+++ src/main/scala/org/drugis/mtc/yadas/RealEffectArgumentMaker.scala
@@ -0,0 +1,45 @@
+package org.drugis.mtc.yadas
+
+import org.drugis.mtc._
+import gov.lanl.yadas.ArgumentMaker
+
+/**
+ * ArgumentMaker for individual treatment effects within studies.
+ * theta_i,k = mu_i + delta_i,b(i),k
+ */
+class RealEffectArgumentMaker(model: NetworkModel,
+		sIdx: Int, dIdx: Int)
+extends ArgumentMaker {
+	/**
+	 * Calculate "the argument": an array of succes-probabilities, one for
+	 * each study-arm.
+	 * data[sIdx] should contain study baseline means, one per study
+	 * data[dIdx] should contain relative effects, one for each non-baseline
+	 */
+	def getArgument(data: Array[Array[Double]]): Array[Double] = {
+		Array.make(0, 0.0) ++ {
+			for {d <- model.data} yield prob(d._1, d._2.treatment, data)
+		}
+	}
+
+	private def relativeTreatmentIndex(s: Study, t: Treatment): Int = {
+		model.studyRelativeEffects(s).findIndexOf(x => x._2 == t)
+	}
+
+	private def treatmentIndex(s: Study, t: Treatment): Int = {
+		val base = model.relativeEffectIndex(s)
+		if (model.studyBaseline(s) == t) -1
+		else base + relativeTreatmentIndex(s, t)
+	}
+
+	private def prob(s: Study, t: Treatment, data: Array[Array[Double]])
+	: Double = {
+		val baselineIdx = model.studyList.indexOf(s)
+		val treatmentIdx = treatmentIndex(s, t)
+
+		if (treatmentIdx >= 0) 
+			data(sIdx)(baselineIdx) + data(dIdx)(treatmentIdx)
+		else
+			data(sIdx)(baselineIdx)
+	}
+}
