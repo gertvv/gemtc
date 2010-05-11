adcc38a74b1cf62f8738409710a5139a5c7d6880
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-05-11 14:50:45 +0200

    Computational test for spanning tree search

---
+++ src/main/scala/org/drugis/mtc/SearchTest.scala
@@ -0,0 +1,34 @@
+package org.drugis.mtc
+
+class SearchTestImpl(xmlFile: String) {
+	val xml = scala.xml.XML.loadFile(xmlFile)
+	val network = Network.fromXML(xml)
+	val top = network.treatments.toList.sort((a, b) => a < b).first 
+
+	def run() {
+		val t0 = System.currentTimeMillis()
+		val result = network.searchSpanningTree(top)
+		val best = result._1
+		val model = NetworkModel(network, best)
+		val t1 = System.currentTimeMillis()
+		println("Enumerated " + result._2)
+		println("ICDF " + model.inconsistencyParameters.size)
+		val v = network.treatmentGraph.vertexSet.size
+		println("|V| " + v)
+		val e = network.treatmentGraph.edgeSet.size
+		println("|E| " + e)
+		val s = e - v + 1
+		println("S " + s)
+		val k = (v * (v - 1)) / 2
+		println("K " + k)
+		val dt = t1 - t0
+		println("dt " + dt)
+	}
+}
+
+object SearchTest {
+	def main(args: Array[String]) {
+		val test = new SearchTestImpl(args(0))
+		test.run()
+	}
+}
