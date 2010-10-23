986036e74545784146d628e7f32859626b9e4efc
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-10-23 15:01:14 +0200

    Add method signature

--- mtc-lib/src/main/java/org/drugis/mtc/summary/RankProbabilitySummary.java
+++ mtc-lib/src/main/java/org/drugis/mtc/summary/RankProbabilitySummary.java
@@ -1,14 +1,21 @@
 package org.drugis.mtc.summary;
 
+import org.drugis.mtc.Treatment;
+import org.drugis.mtc.MCMCResults;
 import org.drugis.mtc.MCMCResultsEvent;
 import org.drugis.mtc.MCMCResultsListener;
+import java.util.List;
 
 // FIXME: this depends on Treatment being available
 public class RankProbabilitySummary implements MCMCResultsListener {
+	public RankProbabilitySummary(MCMCResults results, List<Treatment> treatments) {
+	}
 
 	public void resultsEvent(MCMCResultsEvent event) {
-		// TODO Auto-generated method stub
 
 	}
 
+	public double getValue(Treatment t, int rank) {
+		return 0.0;
+	}
 }
