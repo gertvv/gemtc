ee6e7f7717b626e54bcf3dcce7adb342484fb04b
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-11-04 21:45:22 +0100

    Acceptance test for convergence diagnostic

---
+++ mtc-lib/src/main/java/org/drugis/mtc/convergence/GelmanRubinConvergence.java
@@ -0,0 +1,10 @@
+package org.drugis.mtc.convergence;
+
+import org.drugis.mtc.MCMCResults;
+import org.drugis.mtc.Parameter;
+
+public class GelmanRubinConvergence {
+	public static double diagnose(MCMCResults results, Parameter parameter) {
+		return 2.0;
+	}
+}
---
+++ mtc-lib/src/test/java/org/drugis/mtc/convergence/GelmanRubinAcceptanceTest.java
@@ -0,0 +1,90 @@
+package org.drugis.mtc.convergence;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import org.drugis.mtc.util.FileResults;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.drugis.mtc.*;
+import org.drugis.mtc.yadas.RandomEffectsVariance;
+
+/**
+ * Test for assessment of convergence based on gelman.diag(X) in the R package CODA.
+ * R code is in the "conv.R" resource file.
+ */
+public class GelmanRubinAcceptanceTest {
+	private static final double EPSILON = 0.0000001;
+	private Parameter[] d_parameters;
+	private FileResults d_results;
+
+	@Before
+	public void setUp() throws IOException {
+		InputStream is = GelmanRubinAcceptanceTest.class.getResourceAsStream("conv-samples.txt");
+		Treatment t1 = new Treatment("iPCI");
+		Treatment t2 = new Treatment("mPCI");
+		Treatment t3 = new Treatment("sPCI");
+		d_parameters = new Parameter[] {
+				new BasicParameter(t1, t2), new BasicParameter(t2, t3), new RandomEffectsVariance()	
+		};
+		d_results = new FileResults(is, d_parameters, 3, 10000);
+	}
+	
+	@Test
+	public void testReadFiles() throws IOException {
+		// currently only checks whether the samples can be read. can be removed once the other tests are un-ignored.
+		assertNotNull(readExpected("conv-0.5k.txt"));
+		assertNotNull(readExpected("conv-2k.txt"));
+		assertNotNull(readExpected("conv-10k.txt"));
+	}
+	
+	@Test @Ignore
+	public void testResults500() throws IOException {
+		double[][] expected = readExpected("conv-0.5k.txt");
+		// d_results.makeSamplesAvailable(500); -- not yet implemented
+		assertEquals(expected[0][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[0]), EPSILON);
+		assertEquals(expected[1][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[1]), EPSILON);
+		assertEquals(expected[2][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[2]), EPSILON);
+	}
+	
+	@Test @Ignore
+	public void testResults2k() throws IOException {
+		double[][] expected = readExpected("conv-2k.txt");
+		// d_results.makeSamplesAvailable(2000);
+		assertEquals(expected[0][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[0]), EPSILON);
+		assertEquals(expected[1][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[1]), EPSILON);
+		assertEquals(expected[2][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[2]), EPSILON);
+	}
+	
+	@Test @Ignore
+	public void testResults10k() throws IOException {
+		double[][] expected = readExpected("conv-10k.txt");
+		d_results.makeSamplesAvailable(); // all samples
+		assertEquals(expected[0][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[0]), EPSILON);
+		assertEquals(expected[1][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[1]), EPSILON);
+		assertEquals(expected[2][0], GelmanRubinConvergence.diagnose(d_results, d_parameters[2]), EPSILON);
+	}
+
+	private double[][] readExpected(String file) throws IOException {
+		InputStream is = GelmanRubinAcceptanceTest.class.getResourceAsStream(file);
+		double[][] data = new double[d_parameters.length][2];		
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		reader.readLine(); // skip the first line (column names).
+		for (int i = 0; reader.ready(); ++i) {
+			String line = reader.readLine();
+			StringTokenizer tok = new StringTokenizer(line, ",");
+			tok.nextToken(); // skip the first column (IDs)
+			for (int j = 0; tok.hasMoreTokens(); ++j) {
+				data[i][j] = Double.parseDouble(tok.nextToken());
+			}
+		}
+		return data;
+	}
+}
