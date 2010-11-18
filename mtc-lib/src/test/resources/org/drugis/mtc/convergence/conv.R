fce6a528025f9a4b16ea806bb85d614d7e9a098c
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-11-18 14:14:15 +0100

    Convergence diagnostics: make testfile R-readable, correct conv-10k.txt

--- mtc-lib/src/test/resources/org/drugis/mtc/convergence/conv.R
+++ mtc-lib/src/test/resources/org/drugis/mtc/convergence/conv.R
@@ -16,3 +16,14 @@ write.table(convSample[[3]], file="samples.txt", append=TRUE, col.names=FALSE, s
 write.table(gelman.diag(convSample)$psrf, file="results-10k.txt", sep=",")
 write.table(gelman.diag(lapply(convSample, function(x) { as.mcmc(x[1:2000,]) }))$psrf, file="results-2k.txt", sep=",")
 write.table(gelman.diag(lapply(convSample, function(x) { as.mcmc(x[1:500,]) }))$psrf, file="results-0.5k.txt", sep=",")
+
+
+### calculation that also gives the correct result for 10k:
+x <- read.table("conv-samples.txt", sep=",")
+c1 <- x[1:10000,]
+c2 <- x[10001:20000,]
+c3 <- x[20001:30000,]
+m <- mcmc.list(as.mcmc(c1), as.mcmc(c2), as.mcmc(c3))
+write.table(gelman.diag(m)$psrf, file="conv-10k.txt", sep=",")
+write.table(gelman.diag(window(m, end=2000))$psrf, file="conv-2k.txt", sep=",")
+write.table(gelman.diag(window(m, end=500))$psrf, file="conv-0.5k.txt", sep=",")
