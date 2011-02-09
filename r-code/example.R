0860fb5f5e2285282fa1bf025a1d15a90aac9d6e
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-02-09 13:00:03 +0100

    r-code: now packaged as R library

--- r-code/example.R
+++ r-code/example.R
@@ -1,8 +1,8 @@
-source('mtc.rjava.R')
-network <- mtcNetwork("../example/luades-smoking.xml")
+library(mtc)
+
+network <- mtcNetwork("luades-smoking.xml")
 model <- mtcModel(network)
 data <- mtcJags(model, 3000, 2000)
 
-source('mtc.R')
 data <- append.derived(data, model$jags$analysis$deriv)
 summary(data)
