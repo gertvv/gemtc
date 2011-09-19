1952fd347058d1dfae53f1937d9988de834c7bd0
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-09-19 16:33:51 +0200

    Clean out unused code from JagsSyntaxModel (story #354)

--- mtc-model/src/main/scala/org/drugis/mtc/jags/JagsSyntaxModel.scala
+++ mtc-model/src/main/scala/org/drugis/mtc/jags/JagsSyntaxModel.scala
@@ -40,6 +40,8 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 			throw new IllegalStateException("Unknown measurement type " + cls)
 	}
 
+	val isJags = true
+
 	val format = new DecimalFormat("0.0##E0")
 
 	val inconsistency: Boolean = model.parametrization match {
@@ -47,52 +49,9 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 		case _ => false
 	}
 
-	def dataText: String = jagsDataText
-
-	private def studyIndexVector: List[Int] =
-		model.data.map(a => model.studyMap(a._1))
-
-	private def treatmentIndexVector: List[Int] =
-		model.data.map(a => model.treatmentMap(a._2.treatment))
-
-	private def meanVector: List[Double] =
-		model.data.map(a => a._2.asInstanceOf[ContinuousMeasurement].mean)
-
-	private def errorVector: List[Double] =
-		model.data.map(a => a._2.asInstanceOf[ContinuousMeasurement].stdErr)
-
-	private def responderVector: List[Int] = 
-		model.data.map(a => a._2.asInstanceOf[DichotomousMeasurement].responders)
-
-	private def sampleSizeVector: List[Int] =
-		model.data.map(a => a._2.sampleSize)
-
-	private def baselineVector: List[Int] = 
-		model.studyList.map(s => model.treatmentMap(model.studyBaseline(s)))
-
-	private def vectorStr(name: String, vector: List[_]): String = {
-		name + " <- c(" + vector.mkString(", ") + ")"
-	}
-
 	private def rewrite(s: String): String = s.replaceFirst("E", "*10^")
 	private val varPrior = rewrite(format.format(model.variancePrior))
 	private val effPrior = rewrite(format.format(1/model.normalPrior))
-
-	def modelText: String = bugsSyntaxModel
-	/*def modelText: String = {
-		List(
-			header,
-			baselineEffects,
-			empty,
-			deltas,
-			empty,
-			individualEffects,
-			empty,
-			metaParameters,
-			empty,
-			varianceParameters,
-			footer).mkString("\n")
-	}*/
 
 	def initialValuesText(gen: StartingValueGenerator[M]): String = {
 		List(
@@ -102,6 +61,7 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 			initVarianceParameters(gen)).mkString("\n")
 	}
 
+	val empty = ""
 
 	def scriptText(prefix: String, chains: Int, tuning: Int, simulation: Int)
 	: String = 
@@ -138,135 +98,13 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 		(paramNames ++ varNames).map(p => "monitor " + p).mkString("\n")
 
 	private def paramNames = model.parameterVector.map(p => p.toString)
+
 	private def varNames =
 		if (inconsistency) {
 			List("sd.d", "sd.w")
 		} else {
 			List("sd.d")
 		}
-
-	private val header = "model {"
-	private val empty = ""
-	private val footer = "}"
-	private val baselineEffects = 
-	"""	|	# Study baseline effects
-		|	for (i in 1:length(b)) {
-		|		mu[i] ~ dnorm(0, XXX)
-		|	}""".stripMargin.replace("XXX", effPrior)
-	private val individualEffects = 
-		if (dichotomous)
-	"""	|	# For each (study, treatment), model effect
-		|	for (i in 1:length(s)) {
-		|		logit(p[s[i], t[i]]) <- mu[s[i]] + delta[s[i], b[s[i]], t[i]]
-		|		r[i] ~ dbin(p[s[i], t[i]], n[i])
-		|	}""".stripMargin
-		else
-	"""	|	# For each (study, treatment), model effect
-		|	for (i in 1:length(s)) {
-		|		p[s[i], t[i]] <- mu[s[i]] + delta[s[i], b[s[i]], t[i]]
-		|		m[i] ~ dnorm(p[s[i], t[i]], 1 / (e[i] * e[i]))
-		|	}""".stripMargin
-
-	private def deltas: String = 
-		(for {
-			study <- model.studyList
-		} yield studyDeltas(study)).mkString("\n")
-
-
-	private def studyDeltas(study: Study[M]): String = {
-		if (study.treatments.size == 2) twoArmDeltas(study)
-		else multiArmDeltas(study)
-	}
-
-	private def nonBaselineList(study: Study[M]) = {
-		(study.treatments - base(study)).toList.sorted
-	}
-
-	private def twoArmDeltas(study: Study[M]) = {
-		val treatments = nonBaselineList(study)
-		List(
-			"\t# Random effects in study " + study.id,
-			"\tre[" + idx(study) + ", 1] ~ " +
-				normal(express(study, subj(study)), "tau.d"),
-			"\t" + zeroDelta(study, base(study)),
-			deltasArray(study, treatments, 1)
-		).mkString("\n")
-	}
-
-	private def subj(study: Study[M]) = {
-		require(study.treatments.size == 2)
-		(study.treatments - base(study)).toList.first
-	}
-
-	private def multiArmDeltas(study: Study[M]) = {
-		val treatments = nonBaselineList(study)
-		model.splitNode(study) match {
-			case None => {
-				List(
-					"\t# Random effects in study " + study.id,
-					paramArray(study, treatments),
-					randomEffectArray(study, treatments.size, 1),
-					"\t" + zeroDelta(study, base(study)),
-					deltasArray(study, treatments, 1)).mkString("\n")
-			}
-			case Some(p) => {
-				val t = p._2;
-				{List("\t# Random effects in study " + study.id) ++ {
-					if (treatments.size == 2) Nil
-					else List(paramArray(study, treatments - t))
-				} ++ List(
-					"\tre[" + idx(study) + ", 1] ~ " +
-						normal(express(study, t), "tau.d"),
-					nonSplitEffectArray(study, t),
-					"\t" + zeroDelta(study, base(study)),
-					"\t" + delta(study, t) + " <- " +
-						"re[" + idx(study) + ", 1]",
-					deltasArray(study, treatments - t, 2))
-				}.mkString("\n")
-			}
-		}
-	}
-
-	private def nonSplitEffectArray(study: Study[M], t: Treatment)
-	: String = {
-		val treatments = nonBaselineList(study) - t
-		if (treatments.size == 1) {
-			"\tre[" + idx(study) + ", 2] ~ " +
-				normal(express(study, treatments(0)), "tau.d")
-		} else {
-			randomEffectArray(study, treatments.size, 2)
-		}
-	}
-
-	private def paramArray(study: Study[M], treatments: List[Treatment]) = 
-		(for {
-			k <- 1 to treatments.size
-		} yield "\td[" + idx(study) + ", " + k + "] <- " +
-			express(study, treatments(k - 1))).mkString("\n")
-
-	private def randomEffectArray(study: Study[M], n: Int, n0: Int) = 
-		"\tre[" + idx(study) + ", " + n0 + ":" + (n0 + n - 1) + "] ~ " +
-			"dmnorm(d[" + idx(study) + ", 1:" + n + "], tau." + n + ")"
-
-	private def deltasArray(study: Study[M], treatments: List[Treatment], k0: Int) = 
-		(for {k <- 1 to treatments.size} yield
-			"\t" + delta(study, treatments(k - 1)) + " <- " +
-			"re[" + idx(study) + ", " + (k + k0 - 1) + "]").mkString("\n")
-
-	private def normal(mean: String, tau: String) =
-		"dnorm(" + mean + ", " + tau + ")"
-
-	private def zeroDelta(study: Study[M], subj: Treatment) =
-		delta(study, subj) + " <- 0"
-
-	private def delta(study: Study[M], subj: Treatment) = "delta[" +
-		idx(study) + ", " + idx(base(study)) + ", " + idx(subj) + "]"
-
-	private def idx(study: Study[M]) = model.studyMap(study)
-
-	private def idx(treatment: Treatment) = model.treatmentMap(treatment)
-
-	private def base(study: Study[M]) = model.studyBaseline(study)
 
 	private def expressParam(p: NetworkModelParameter, v: Int,
 		f: String => String): String = 
@@ -289,12 +127,6 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 		require(effect != base)
 		expressParams(model.parametrization(base, effect))
 	}
-
-	private def metaParameters: String =
-		(
-			List("\t# Meta-parameters") ++
-			(for {param <- model.parameterVector} yield format(param))
-		).mkString("\n")
 
 	private def asBasic(p: NetworkModelParameter): BasicParameter = p match {
 		case b: BasicParameter => b
@@ -331,21 +163,13 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 
 	private def initRelativeEffects(g: StartingValueGenerator[M]): String = {
 		"`delta` <- " + JagsSyntaxModel.writeMatrix(model.studyList.map(
-				s => studyArms(s).map(init(s, _, g))), true)
+				s => studyArms(s).map(init(s, _, g))), isJags)
 	}
 
 	private def init(s: Study[M], t: Treatment, g: StartingValueGenerator[M])
 	: java.lang.Double = {
-		if (t == null || t == base(s)) null
-		else g.getRandomEffect(s, new BasicParameter(base(s), t))
-	}
-
-	private def init(s: Study[M], idx: Int, g: StartingValueGenerator[M])
-	: String = {
-		if (idx < s.treatments.size - 1) {
-			val p = new BasicParameter(base(s), nonBaselineList(s)(idx))
-			g.getRandomEffect(s, p).toString
-		} else "NA"
+		if (t == null || t == model.studyBaseline(s)) null
+		else g.getRandomEffect(s, new BasicParameter(model.studyBaseline(s), t))
 	}
 
 	private def initVarianceParameters(g: StartingValueGenerator[M]): String = {
@@ -355,67 +179,6 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 			else ""
 		} + "`sd.d` <- " + g.getRandomEffectsVariance()
 	}
-
-	private def format(p: NetworkModelParameter): String = 
-		"\t" + p.toString + " ~ " + normal("0", variance(p))
-
-	private def variance(p: NetworkModelParameter): String = p match {
-		case b: BasicParameter => effPrior 
-		case s: SplitParameter => effPrior 
-		case i: InconsistencyParameter => "tau.w"
-		case _ => throw new RuntimeException("Unhandled Parameter type")
-	}
-
-	private def basicVar(name: String) = (
-		"""	|	sd.x ~ dunif(0, """ + varPrior + """)
-			|	var.x <- sd.x * sd.x
-			|	tau.x <- 1 / var.x""").stripMargin.replaceAll("x", name)
-
-	private def varDim(s: Study[M]): Int = {
-		model.splitNode(s) match {
-			case Some(_) => s.treatments.size - 2 
-			case None => s.treatments.size - 1
-		}
-	}
-
-	private def combinedVars: List[String] = 
-		for {
-			n <- model.network.studies.map(a => varDim(a)).toList;
-			if (n > 1)
-		} yield "\n" + tauMatrix(n)
-
-	private def tauMatrix(dim: Int)  =
-		if (dim == 2)
-			"""	|	# 2x2 inv. covariance matrix for 3-arm trials
-				|	var.2[1, 1] <- var.d
-				|	var.2[1, 2] <- var.d / 2
-				|	var.2[2, 1] <- var.d / 2
-				|	var.2[2, 2] <- var.d
-				|	tau.2 <- inverse(var.2)""".stripMargin
-		else throw new Exception("Studies with > 3 arms not supported yet; Please email Gert van Valkenhoef <g.h.m.van.valkenhoef at rug.nl>")
-
-	private def varianceParameters: String =
-		if (inconsistency) {
-			List(
-				inconsistencyVariance,
-				empty,
-				randomEffectsVariance
-			).mkString("\n")
-		} else {
-			randomEffectsVariance
-		}
-
-	private def inconsistencyVariance: String =
-		List(
-			"\t# Inconsistency variance",
-			basicVar("w")).mkString("\n")
-
-	private def randomEffectsVariance: String = 
-		(List(
-			"\t# Random effect variance",
-			basicVar("d")
-		) ++ combinedVars).mkString("\n")
-
 
 	private def derivations = {
 		val n = model.treatmentList.size
@@ -445,7 +208,7 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 		TemplateCompiler.compileTemplate(getClass().getResourceAsStream(fileName), null)
 	}
 
-	def bugsSyntaxModel: String = {
+	def modelText: String = {
 		val template = readTemplate("modelTemplate.txt")
 		val map = new java.util.HashMap[String, Object]()
 		map.put("dichotomous", dichotomous.asInstanceOf[AnyRef])
@@ -501,21 +264,21 @@ class JagsSyntaxModel[M <: Measurement, P <: Parametrization[M]](
 		model.studyList.map(x => x.treatments.size.asInstanceOf[java.lang.Integer])
 	}
 
-	def jagsDataText: String = 
+	def dataText: String = 
 		if (dichotomous) {
 			List(
 				"ns <- " + JagsSyntaxModel.writeNumber(model.studyList.size.asInstanceOf[java.lang.Integer]),
-				"t <- " + JagsSyntaxModel.writeMatrix(treatmentMatrix, true),
-				"r <- " + JagsSyntaxModel.writeMatrix(responderMatrix, true),
-				"n <- " + JagsSyntaxModel.writeMatrix(sampleSizeMatrix, true),
+				"t <- " + JagsSyntaxModel.writeMatrix(treatmentMatrix, isJags),
+				"r <- " + JagsSyntaxModel.writeMatrix(responderMatrix, isJags),
+				"n <- " + JagsSyntaxModel.writeMatrix(sampleSizeMatrix, isJags),
 				"na <- " + JagsSyntaxModel.writeVector(armCounts)
 			).mkString("\n")
 		} else {
 			List(
 				"ns <- " + JagsSyntaxModel.writeNumber(model.studyList.size.asInstanceOf[java.lang.Integer]),
-				"t <- " + JagsSyntaxModel.writeMatrix(treatmentMatrix, true),
-				"m <- " + JagsSyntaxModel.writeMatrix(meanMatrix, true),
-				"e <- " + JagsSyntaxModel.writeMatrix(stdErrMatrix, true),
+				"t <- " + JagsSyntaxModel.writeMatrix(treatmentMatrix, isJags),
+				"m <- " + JagsSyntaxModel.writeMatrix(meanMatrix, isJags),
+				"e <- " + JagsSyntaxModel.writeMatrix(stdErrMatrix, isJags),
 				"na <- " + JagsSyntaxModel.writeVector(armCounts)
 			).mkString("\n")
 		}
