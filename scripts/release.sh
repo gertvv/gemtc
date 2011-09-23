a06cb854b498aa503aea60664fbf6ef1f616687f
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-09-23 15:03:54 +0200

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,8 @@ mvn validate
 
 VERSION=`cat version`
 rm version
-GUIDIR=mtc-gui-$VERSION
-CLIDIR=mtc-gui-$VERSION
+GUIDIR=gemtc-gui-$VERSION
+CLIDIR=gemtc-cli-$VERSION
 
 if [ "$VERSION" = '' ]; then
 	echo '!!!! Error: could not get version';
@@ -26,9 +26,10 @@ ant license || exit
 
 # Package
 echo '---- Building JAR'
-mvn clean package -q|| exit
+#mvn clean package -q|| exit
 
 # Package the GUI
+rm -Rf $GUIDIR
 mkdir $GUIDIR
 ARTIFACT=$GUIDIR/gemtc-gui-$VERSION.jar
 cp mtc-gui/target/mtc-gui-$VERSION-jar-with-dependencies.jar $ARTIFACT
@@ -38,9 +39,10 @@ cp mtc-gui/README.txt $GUIDIR
 mkdir $GUIDIR/example
 cp example/*.gemtc $GUIDIR/example/
 cp r-code/mtc/R/mtc.R $GUIDIR/
-zip -r mtc-gui-$VERSION.zip $GUIDIR
+zip -r gemtc-gui-$VERSION.zip $GUIDIR
 
 # Package the CLI
+rm -Rf $CLIDIR
 mkdir $CLIDIR
 ARTIFACT=$CLIDIR/gemtc-cli-$VERSION.jar
 cp mtc-cli/target/mtc-cli-$VERSION-jar-with-dependencies.jar $ARTIFACT
@@ -50,7 +52,7 @@ cp mtc-cli/README.txt $CLIDIR
 mkdir $CLIDIR/example
 cp example/*.gemtc $CLIDIR/example/
 cp r-code/mtc/R/mtc.R $CLIDIR/
-zip -r mtc-cli-$VERSION.zip $CLIDIR
+zip -r gemtc-cli-$VERSION.zip $CLIDIR
 
 # Package the R package
 cd r-code
