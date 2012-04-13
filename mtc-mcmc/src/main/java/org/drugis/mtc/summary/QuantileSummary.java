2970d74e79b03692139c3c080860f02c5c253c79
Joel Kuiper <me@joelkuiper.eu>
2012-04-13 11:37:29 +0200

    Revert "Added boolean for being continuous in QuantileSummary"
    This reverts commit 8dd8afd792df7d9c7d72e072a80096fd275936d8.

--- mtc-mcmc/src/main/java/org/drugis/mtc/summary/QuantileSummary.java
+++ mtc-mcmc/src/main/java/org/drugis/mtc/summary/QuantileSummary.java
@@ -42,7 +42,6 @@ public class QuantileSummary extends AbstractObservable implements MCMCResultsLi
 	private MCMCResults d_results;
 	private double[] d_quantiles;
 	private boolean d_defined = false;
-	private boolean d_continuous = false; 
 
 	public QuantileSummary(MCMCResults results, Parameter parameter, double[] probabilities) {
 		this.d_probabilities = probabilities;
@@ -132,13 +131,5 @@ public class QuantileSummary extends AbstractObservable implements MCMCResultsLi
 	
 	public boolean getDefined() {
 		return d_defined;
-	}
-
-	public boolean isContinuous() {
-		return d_continuous;
-	}
-
-	public void setContinuous(boolean continuous) {
-		d_continuous = continuous;
 	}
 }
