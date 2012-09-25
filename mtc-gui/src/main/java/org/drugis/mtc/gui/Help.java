f90ec2b7d6e72ec6f70350bfc22dd4e167263e29
Joel Kuiper <me@joelkuiper.eu>
2012-09-25 15:46:33 +0200

    Move properties loader for help texts to drugis.common.gui

--- mtc-gui/src/main/java/org/drugis/mtc/gui/Help.java
+++ mtc-gui/src/main/java/org/drugis/mtc/gui/Help.java
@@ -19,32 +19,12 @@
 
 package org.drugis.mtc.gui;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import org.drugis.common.gui.HelpLoader;
 
 public class Help {
-	private static final String TEXT_NOT_FOUND_ERROR = "<p><b>Error: </b>Help text not found.</p>";
-	private static final String HELP_FILE = "help.properties";
-	private static Properties s_properties = null;
+	private static HelpLoader s_help = new HelpLoader(Help.class.getResourceAsStream("help.properties"));
 
 	public static String getHelpText(String key) {
-		if (s_properties == null) {
-			initialize();
-		}
-		return s_properties.getProperty(key, TEXT_NOT_FOUND_ERROR);
-	}
-
-	private static void initialize() {
-		Properties properties = new Properties();
-		try {
-			InputStream is = Help.class.getResourceAsStream(HELP_FILE);
-			properties.load(is);
-			is.close();
-		} catch (IOException e) {
-			s_properties = properties;
-			throw new RuntimeException("Could not initialize GeMTC help text.", e);
-		}
-		s_properties = properties;
+		return s_help.getHelpText(key);
 	}
 }
