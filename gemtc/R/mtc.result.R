52117feb91990110e643e70d362bdf1c0b4d6f06
Joel Kuiper <me@joelkuiper.eu>
2013-02-15 16:48:53 +0100

    Properly check parameters before forest plot (fix #626)
     - Also automatically group on baseline in forest()

--- gemtc/R/mtc.result.R
+++ gemtc/R/mtc.result.R
@@ -14,16 +14,24 @@ plot.mtc.result <- function(x, ...) {
 
 forest.mtc.result <- function(x, ...) { 
     quantiles <- summary(x)$quantiles 
-    stats <- quantiles[-dim(quantiles)[1],]
+    stats <- quantiles[grep("^d\\.", rownames(quantiles)),]
     if(class(stats) == "numeric") { # Selecting a single row returns a numeric 
         stats <- as.matrix(t(stats))
         row.names(stats) <- row.names(quantiles)[[1]]
     }
-    data <- data.frame(id=rownames(stats), pe=stats[,3], ci.l=stats[,1], ci.u=stats[,5], group=NA, style="normal")
+    groups <- extract.comparisons(rownames(quantiles))[,1]
+    group.names <- unique(groups)
+    group.labels <- rep("", length(group.names))
+    #group.labels <- paste("Relative to ", group.names)
+    names(group.labels) <- group.names
+    data <- data.frame(
+        id=rownames(stats),
+        pe=stats[,3], ci.l=stats[,1], ci.u=stats[,5],
+        group=groups, style="normal")
     blobbogram(data,
         columns=c(), column.labels=c(),
         id.label="Comparison", ci.label="Odds Ratio (95% CrI)", log.scale=TRUE,
-        grouped=FALSE)
+        grouped=length(group.labels)>1, group.labels=group.labels)
 }
 
 as.mcmc.list.mtc.result <- function(x, ...) {
