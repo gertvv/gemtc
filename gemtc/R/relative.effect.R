d15773156825b57342e958a2b3a9dcf0d21a22f9
Joel Kuiper <me@joelkuiper.eu>
2013-02-15 16:47:42 +0100

    Fix relative effect bugs with few variables (fix #629)

--- gemtc/R/relative.effect.R
+++ gemtc/R/relative.effect.R
@@ -6,7 +6,13 @@ tree.relative.effect <- function(g, t1, t2) {
     if(length(t2) > length(t1)) t1 <- rep(t1, length.out=length(t2))
     pairs <- matrix(c(t1, t2), ncol=2)
     paths <- apply(pairs, 1, function(rel) {
-        p <- unlist(get.shortest.paths(g, rel[1], rel[2], mode='all'))
+        p <- unlist(suppressWarnings(get.shortest.paths(g, rel[1], rel[2], mode='all')))
+        if (length(p) == 0) {
+            stop(paste("The requested comparison ",
+                       V(g)[rel[2]]$name, " vs ", V(g)[rel[1]]$name,
+                       " can not be expressed as a linear combination of the ",
+                       "available parameters", sep=""))
+        }
         p <- matrix(c(p[1:length(p)-1], p[-1]), ncol=2)
         edges <- sapply(E(g), function(e) {
             v <- get.edge(g, e)
@@ -15,7 +21,11 @@ tree.relative.effect <- function(g, t1, t2) {
             else 0
         })
     })
-    paths <- as.matrix(paths) # necessary when there is only 1
+
+    # Ensure paths is a matrix, since apply() will simplify to a vector if
+    # either ncol==1 or nrow==1
+    paths <- matrix(as.numeric(paths), ncol=length(t1), nrow=length(E(g)))
+
     colnames(paths) <-  apply(pairs, 1, function(pair) { 
         pair <- V(g)[pair]$name
         paste('d', pair[1], pair[2], sep='.')
@@ -23,12 +33,29 @@ tree.relative.effect <- function(g, t1, t2) {
     paths
 }
 
+# Given a vector of parameters names (d.A.B, sd.d, etc.), extract all basic
+# parameters and generate a two-column matrix in which each row is a comparison
+extract.comparisons <- function(parameters) {
+    x <- parameters[grep("^d\\.", parameters)]
+    matrix(unlist(strsplit(x, "\\.")), ncol=3, byrow=TRUE)[,-1]
+}
+
+spanning.tree.mtc.result <- function(result) {
+    network <- result$model$network
+    pairs <- extract.comparisons(colnames(result$samples[[1]]))
+    parameters <- data.frame(
+        t1=as.treatment.factor(pairs[,1], network),
+        t2=as.treatment.factor(pairs[,2], network)
+    )
+    graph.create(network$treatments$id, parameters, arrow.mode=2, color="black", lty=1)
+}
+
 relative.effect <- function(result, t1, t2 = c(), preserve.extra=TRUE) {
     if(result$model$type != "Consistency") stop("Cannot apply relative.effect to this model")
 
     # Build relative effect transformation matrix
     network <- result$model$network
-    g <- result$model$tree
+    g <- spanning.tree.mtc.result(result)
     if (is.character(t1)) {
         t1 <- as.treatment.factor(t1, network)
     }
--- gemtc/inst/tests/test_relative.effect.R
+++ gemtc/inst/tests/test_relative.effect.R
@@ -64,3 +64,37 @@ test_that("tree.relative.effect handles a more complex tree", {
       ))
   expect_that(tree.relative.effect(tree, t1="tPA", t2="Ten"), is_equivalent_to(expected))
 })
+
+test_that("tree.relative.effect handles two-treatment case", {
+  g <- graph.edgelist(matrix(c("A", "B"), ncol=2))
+  expected <- matrix(-1, dimnames=list(NULL, c("d.B.A")))
+  expect_that(tree.relative.effect(g, t1=2, t2=c()), equals(expected))
+
+  expected <- matrix(c(0, 1), dimnames=list(NULL, c("d.A.A", "d.A.B")), ncol=2)
+  expect_that(tree.relative.effect(g, t1=1, t2=c(1, 2)), equals(expected))
+})
+
+test_that("relative.effect can be applied recursively", {
+  result <- dget(system.file("extdata/luades-smoking.samples.gz", package="gemtc"))
+  result <- relative.effect(result, "C")
+  stats <- summary(relative.effect(result, "B"))
+
+  expected <- textConnection('
+           Mean     SD  Naive.SE Time-series.SE
+  d.B.A -0.4965 0.4081 0.004563       0.004989
+  d.B.C  0.3394 0.4144 0.004634       0.004859
+  d.B.D  0.6123 0.4789 0.005354       0.005297
+  sd.d   0.8465 0.1913 0.002139       0.002965
+  ')
+  expected <- as.matrix(read.table(expected, header=TRUE))
+  colnames(expected)[3] <- "Naive SE"
+  colnames(expected)[4] <- "Time-series SE"
+  expect_that(stats$statistics, equals(expected, tolerance=0.00005, scale=1))
+})
+
+test_that("relative.effect.tree throws an error if requested comparison is not connected", {
+  g <- graph.edgelist(t(matrix(c("A", "B", "A", "C"), nrow=2)))
+  g <- g + vertex("D")
+
+  expect_error(tree.relative.effect(g, t1=1, t2=4))
+})
