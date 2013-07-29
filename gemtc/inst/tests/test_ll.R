a26c17647ba39acfb013c423a79c86d951a04bd0
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-07-29 17:14:04 +0200

    Test likelihood/links are fully implemented

---
+++ gemtc/inst/tests/test_ll.R
@@ -0,0 +1,18 @@
+context("likelihood/link");
+
+# This file contains a single test for each implemented likelihood.
+# Since ll.defined() checks whether all required methods are implemented,
+# this is a simple guard against forgetting to add or correctly name new
+# methods.
+
+test_that("normal.identity is defined", {
+  expect_true(ll.defined(list(likelihood='normal', link='identity')))
+})
+
+test_that("binom.logit is defined", {
+  expect_true(ll.defined(list(likelihood='binom', link='logit')))
+})
+
+test_that("binom.cloglog is defined", {
+  expect_true(ll.defined(list(likelihood='binom', link='cloglog')))
+})
