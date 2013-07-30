cb10a290a5be5a022c58b3b21bac38829881b891
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-07-30 14:25:59 +0200

    Add unit tests for model$data
    This was broken at some previous stage.

--- gemtc/inst/tests/test_mtc.model_data.R
+++ gemtc/inst/tests/test_mtc.model_data.R
@@ -30,3 +30,116 @@ study treatment responders sampleSize
   model <- list(network=network, likelihood='binom', link='logit', om.scale=2.5)
   expect_error(mtc.model.data(model), 'data.ab contains NAs in column "sampleSize"')
 })
+
+test_that("mtc.model.data includes correct fields for data.ab, binom.logit", {
+  data.ab <- read.table(textConnection('
+study treatment responders sampleSize exposure
+1     A         3          10         20
+1     B         4          12         24'), header=T)
+  network <- mtc.network(data.ab)
+  model <- list(network=network, likelihood='binom', link='logit', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'r', 'n'))
+  ))
+})
+
+test_that("mtc.model.data includes correct fields for data.ab, binom.cloglog", {
+  data.ab <- read.table(textConnection('
+study treatment responders sampleSize exposure
+1     A         3          10         20
+1     B         4          12         24'), header=T)
+  network <- mtc.network(data.ab)
+  model <- list(network=network, likelihood='binom', link='cloglog', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'r', 'n'))
+  ))
+})
+
+test_that("mtc.model.data includes correct fields for data.ab, poisson.log", {
+  data.ab <- read.table(textConnection('
+study treatment responders sampleSize exposure
+1     A         3          10         20
+1     B         4          12         24'), header=T)
+  network <- mtc.network(data.ab)
+  model <- list(network=network, likelihood='poisson', link='log', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'r', 'E'))
+  ))
+})
+
+test_that("mtc.model.data includes correct fields for data.ab, normal.identity", {
+  data.ab <- read.table(textConnection('
+study treatment mean std.err
+1     A         3.4  0.21
+1     B         4.0  0.19'), header=T)
+  network <- mtc.network(data.ab)
+  model <- list(network=network, likelihood='normal', link='identity', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'm', 'e'))
+  ))
+})
+
+test_that("mtc.model.data includes correct fields for data.re", {
+  data.re <- read.table(textConnection('
+study treatment diff std.err
+1     A         NA   0.21
+1     B         4.0  0.19'), header=T)
+  network <- mtc.network(data.re=data.re)
+  model <- list(network=network, likelihood='normal', link='identity', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'm', 'e'))
+  ))
+})
+
+test_that("mtc.model.data includes correct fields for data.ab, normal.identity + data.re", {
+  data.ab <- read.table(textConnection('
+study treatment mean std.err
+1     A         3.4  0.21
+1     B         4.0  0.19'), header=T)
+  data.re <- read.table(textConnection('
+study treatment diff std.err
+2     A         NA   0.21
+2     B         4.0  0.19'), header=T)
+  network <- mtc.network(data.ab, data.re=data.re)
+  model <- list(network=network, likelihood='normal', link='identity', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'm', 'e'))
+  ))
+})
+
+test_that("mtc.model.data includes correct fields for data.ab, binom.logit + data.re", {
+  data.ab <- read.table(textConnection('
+study treatment responders sampleSize
+1     A         3          10
+1     B         4          12'), header=T)
+  data.re <- read.table(textConnection('
+study treatment diff std.err
+2     A         NA   0.21
+2     B         4.0  0.19'), header=T)
+  network <- mtc.network(data.ab, data.re=data.re)
+  model <- list(network=network, likelihood='binom', link='logit', om.scale=2.5)
+  data <- mtc.model.data(model)
+  expect_that(sort(names(data)), equals(
+    sort(c(
+      'ns.a', 'ns.r2', 'ns.rm', 'ns', 'nt', 'om.scale',
+      'na', 't', 'r', 'n', 'm', 'e'))
+  ))
+})
