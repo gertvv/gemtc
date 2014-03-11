5143eae74e09d9851387b2f85fc65a713a7ecde6
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-03-11 09:52:50 +0100

    Fix incorrect validation test

--- gemtc/tests/testthat/test-validate-smoking-ume.R
+++ gemtc/tests/testthat/test-validate-smoking-ume.R
@@ -1,7 +1,7 @@
 context("[validate] Modified from NICE TSD4 smoking example")
 
 test_that("The summaries match", {
-  network <- mtc.network(data.re=read.table('../data/smoking-ume.data.txt', header=TRUE))
-  result <- replicate.example("smoking-ume", network, likelihood="binom", link="cloglog", type="ume")
+  network <- mtc.network(data.ab=read.table('../data/smoking-ume.data.txt', header=TRUE))
+  result <- replicate.example("smoking-ume", network, likelihood="binom", link="logit", type="ume")
   compare.summaries(result$s1, result$s2)
 })
