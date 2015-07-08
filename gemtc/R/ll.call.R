ba94e6a43b1e962cf55700b04b6601fae8a226d6
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-07-08 15:38:55 +0200

    Fix requirements for likelihood/link to be defined

--- gemtc/R/ll.call.R
+++ gemtc/R/ll.call.R
@@ -6,7 +6,7 @@ ll.call <- function(fnName, model, ...) {
 ll.defined <- function(model) {
   fns <- c('mtc.arm.mle', 'mtc.rel.mle', 'mtc.code.likelihood',
     'scale.log', 'scale.name', 'scale.limit.inits',
-    'required.columns.ab')
+    'required.columns.ab', 'validate.data')
   fns <- paste(fns, model[['likelihood']], model[['link']], sep=".")
-  all(exists(fns, mode='function'))
+  all(sapply(fns, function(fn) { exists(fn, mode='function') }))
 }
