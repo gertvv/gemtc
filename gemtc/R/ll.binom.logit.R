786d1437a27e638e822ee39f6ec70b3cb7e6e389
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-07-14 17:16:13 +0200

    Implement various corrections for odds ratio

--- gemtc/R/ll.binom.logit.R
+++ gemtc/R/ll.binom.logit.R
@@ -1,15 +1,32 @@
 # Arm-level effect estimate (given a one-row data frame)
 # Returns mean, standard deviation.
-mtc.arm.mle.binom.logit <- function(data) {
-  s <- unname(data['responders'] + 0.5)
-  f <- unname(data['sampleSize'] - s + 1)
+mtc.arm.mle.binom.logit <- function(data, k=0.5) {
+  s <- unname(data['responders'] + k)
+  f <- unname(data['sampleSize'] - s + 2 * k)
   c('mean'=log(s/f), 'sd'=sqrt(1/s + 1/f))
 }
 
 # Relative effect estimate (given a two-row data frame)
-mtc.rel.mle.binom.logit <- function(data) {
-  e1 <- mtc.arm.mle.binom.logit(data[1,])
-  e2 <- mtc.arm.mle.binom.logit(data[2,])
+mtc.rel.mle.binom.logit <- function(data, correction.force=TRUE, correction.type="constant", correction.magnitude=1) {
+  correction.need <-
+    data[1,'responders'] == 0 || data[1,'responders'] == data[1,'sampleSize'] ||
+    data[2,'responders'] == 0 || data[2,'responders'] == data[2,'sampleSize']
+
+  groupRatio <- if (correction.type == "reciprocal") {
+    data[1,'sampleSize'] / data[2,'sampleSize']
+  } else {
+    1
+  }
+
+  correction <- if (correction.force || correction.need) {
+    correction.magnitude * c(groupRatio/(groupRatio+1), 1/(groupRatio+1))
+  } else {
+    c(0, 0)
+  }
+
+  e1 <- mtc.arm.mle.binom.logit(data[1,], k=correction[1])
+  e2 <- mtc.arm.mle.binom.logit(data[2,], k=correction[2])
+
   c(e2['mean'] - e1['mean'], sqrt(e1['sd']^2 + e2['sd']^2))
 }
 
