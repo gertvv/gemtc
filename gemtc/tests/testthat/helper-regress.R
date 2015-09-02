d920eada11b7c584274f8063dd3435da7e801cfb
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-09-02 16:56:26 +0200

    Add deviance expectations to regression tests

--- gemtc/tests/testthat/helper-regress.R
+++ gemtc/tests/testthat/helper-regress.R
@@ -10,6 +10,8 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
   expect_true("sd.d" %in% rownames(s$summaries$quantiles))
   summary(relative.effect(result, t1=network$treatments$id[2]))
   expect_equal(dim(rank.probability(result)), c(n.trt, n.trt))
+  expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+  expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 
   # consistency model, fixed effect
   model <- mtc.model(network, likelihood=likelihood, link=link, linearModel="fixed")
@@ -20,6 +22,8 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
   expect_false("sd.d" %in% rownames(s$summaries$quantiles))
   summary(relative.effect(result, t1=network$treatments$id[2]))
   expect_equal(dim(rank.probability(result)), c(n.trt, n.trt))
+  expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+  expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 
   if (!is.null(t1) && !is.null(t2)) {
     # node-splitting, random effects
@@ -33,6 +37,8 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
     expect_true("d.indirect" %in% rownames(s$summaries$quantiles))
     expect_error(relative.effect(result, t1="C"))
     expect_error(rank.probability(result))
+    expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+    expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 
     # node-splitting, fixed effect
     model <- mtc.model(network, likelihood=likelihood, link=link, type="nodesplit", t1=t2, t2=t1, linearModel="fixed")
@@ -42,6 +48,8 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
     expect_false("sd.d" %in% rownames(s$summaries$quantiles))
     expect_true("d.direct" %in% rownames(s$summaries$quantiles))
     expect_true("d.indirect" %in% rownames(s$summaries$quantiles))
+    expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+    expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
   }
 
   # ume, random effects
@@ -49,12 +57,16 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
   capture.output(result <- mtc.run(model, n.adapt=100, n.iter=500))
   plot(result)
   s <- summary(result)
+  expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+  expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 
   # ume, fixed effect
   suppressWarnings(model <- mtc.model(network, likelihood=likelihood, link=link, type="ume", linearModel="fixed"))
   capture.output(result <- mtc.run(model, n.adapt=100, n.iter=500))
   plot(result)
   s <- summary(result)
+  expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+  expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 
   # anohe, random effects
   capture.output(anohe <- mtc.anohe(network, likelihood=likelihood, link=link, n.adapt=100, n.iter=500))
@@ -62,6 +74,8 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
   anohe.s <- summary(anohe)
   plot(anohe.s)
   capture.output(print(anohe.s))
+  expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+  expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 
   # anohe, fixed effect
   capture.output(anohe <- mtc.anohe(network, likelihood=likelihood, link=link, linearModel="fixed", n.adapt=100, n.iter=500))
@@ -69,4 +83,6 @@ test.regress <- function(network, likelihood, link, t1=NULL, t2=NULL) {
   anohe.s <- summary(anohe)
   plot(anohe.s)
   capture.output(print(anohe.s))
+  expect_equal(names(result$deviance), c("Dbar", "pD", "DIC", "dev.ab", "dev.re", "fit.ab", "fit.re"))
+  expect_true(!all(is.na(unlist(result$deviance[c("Dbar", "pD", "DIC")]))))
 }
